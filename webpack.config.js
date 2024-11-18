8bf58a1ea35b5e6b5c659bb975d50fe598e7965c
Alfonso <alfonsocenteno2@gmail.com>
2024-11-18 16:03:25 -0600

    made the webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: 'http://localhost:3000',  // Or any URL you want to open
+  },
+};
